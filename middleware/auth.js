bdef2d66c6b8522fc8d37884a540e2cc97638d7d
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-04-16 01:32:27 +0530

    Updated header

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const auth = async (req, res, next) => {
     try {
-        console.log(req.headers)
+        console.log(req.headers.authorization)
         const authHeader = req.headers.authorization
 
         if (!authHeader || !authHeader.startsWith('Bearer '))
