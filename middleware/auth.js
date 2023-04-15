724055c28062404deada009e59f9f362e92971b0
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-04-16 01:43:17 +0530

    Updated header

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,12 +5,15 @@ const auth = async (req, res, next) => {
     try {
         console.log(req.headers.authorization)
         const authHeader = req.headers.authorization
-
+        console.log('1')
         if (!authHeader || !authHeader.startsWith('Bearer '))
             return res.status(401).json({ message: 'Authentication is required' })
-
+        console.log('2')
         const token = authHeader.replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded, 'decoded')
+        console.log(token)
+        console.log('3')
 
         req.user = decoded
         next()
